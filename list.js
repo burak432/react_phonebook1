525ea7227cfa875a9dcec6b2c1cb33927044b317
burak atilla <86172767+burak432@users.noreply.github.com>
2022-02-11 18:16:00 +0300

    Add files via upload
    2 | i dont know how to implement filtering function

--- list.js
+++ list.js
@@ -1,8 +1,19 @@
 import react from "react";
+import { useState } from "react";
 
 function Listele({ allEntries }) {
+
+  const [filterText, setfilterText] = useState("");
+
+  const filterFunc = (e) => {
+    setfilterText(e.target.value)
+}
+
   return (
     <div>
+      <div>
+        <input type="text" name="filterInput" value={filterText} onChange={filterFunc}></input>
+      </div>
       <ul>
         {allEntries.map((item, index) => (
           <li key={index}>{item.fullName} {item.phoneNum}</li>
